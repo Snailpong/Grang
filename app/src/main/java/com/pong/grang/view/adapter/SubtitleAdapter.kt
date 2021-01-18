52626ba568fcb302e8cf2a7acb5d38fd23639975
Snailpong <tjdtnsu@naver.com>
2021-01-18 21:06:03 +0900

    add item

--- app/src/main/java/com/pong/grang/view/adapter/SubtitleAdapter.kt
+++ app/src/main/java/com/pong/grang/view/adapter/SubtitleAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.pong.grang.R
 import com.pong.grang.model.SubtitleModel
 
-class SubtitleAdapter(val subtitleItem: ArrayList<SubtitleModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class SubtitleAdapter(val subtitleItems: ArrayList<SubtitleModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_subtitle, parent, false)
         val viewHolder = SubtitleHolder(view)
@@ -14,12 +14,16 @@ class SubtitleAdapter(val subtitleItem: ArrayList<SubtitleModel>): RecyclerView.
         return viewHolder
     }
 
+    fun addItem(subtitleModel: SubtitleModel) {
+        subtitleItems.add(subtitleModel)
+    }
+
     override fun getItemCount(): Int {
-        return subtitleItem.size
+        return subtitleItems.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val subtitleModel = subtitleItem[position]
+        val subtitleModel = subtitleItems[position]
         val subtitleHolder = holder as SubtitleHolder
         subtitleHolder.bind(subtitleModel)
     }
--- app/src/main/java/com/pong/grang/view/ui/PlayerSubtitleActivity.kt
+++ app/src/main/java/com/pong/grang/view/ui/PlayerSubtitleActivity.kt
@@ -2,11 +2,13 @@ package com.pong.grang.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pong.grang.R
 import com.pong.grang.model.SubtitleModel
 import com.pong.grang.view.adapter.SubtitleAdapter
 import kotlinx.android.synthetic.main.activity_player_subtitle.*
+import kotlinx.android.synthetic.main.dialog_add_subtitle.view.*
 
 class PlayerSubtitleActivity : AppCompatActivity() {
 
@@ -16,23 +18,50 @@ class PlayerSubtitleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_subtitle)
 
-        mSubtitleItems.run {
-            add(SubtitleModel(1, 0, 60, "자막1"))
-            add(SubtitleModel(2, 0, 60, "자막2"))
-            add(SubtitleModel(3, 0, 60, "자막3"))
-            add(SubtitleModel(4, 0, 60, "자막4"))
-        }
-
         initRecyclerView()
+        initAddButton()
     }
 
     private fun initRecyclerView() {
         mSubtitleAdapter = SubtitleAdapter(mSubtitleItems)
+
+        mSubtitleItems.run {
+            add(SubtitleModel(1, 0, 60, "자막1"))
+            add(SubtitleModel(2, 60, 90, "자막2"))
+            add(SubtitleModel(3, 90, 120, "자막3"))
+            add(SubtitleModel(4, 120, 150, "자막4"))
+        }
 
         recyclerview_subtitle_list.run {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@PlayerSubtitleActivity)
             adapter = mSubtitleAdapter
         }
+    }
+
+    private fun initAddButton() {
+        btn_add_subtitle.setOnClickListener {
+            openAddSubtitleDialog()
+        }
+    }
+
+    private fun openAddSubtitleDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_add_subtitle, null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("add")
+            .setView(dialogView)
+            .setPositiveButton("Ok", { dialogInterface, i ->
+                val idx = dialogView.idx_dialog_add_subtitle.text.toString().toInt()
+                val startTime = dialogView.start_time_dialog_add_subtitle.text.toString().toLong()
+                val endTime = dialogView.end_time_dialog_add_subtitle.text.toString().toLong()
+                val text = dialogView.text_dialog_add_subtitle.text.toString()
+
+                val subtitleModel = SubtitleModel(idx, startTime, endTime, text)
+                mSubtitleAdapter.addItem(subtitleModel)
+                mSubtitleAdapter.notifyDataSetChanged()
+            })
+            .setNegativeButton("No", null)
+            .create()
+        dialog.show()
     }
 }
