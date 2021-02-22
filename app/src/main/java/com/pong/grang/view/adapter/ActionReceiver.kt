883e1cbb0b95888c86363ca357a4ee1f35d5e3f0
Snailpong <tjdtnsu@gmail.com>
2021-02-22 21:30:44 +0900

    Refactor: Log Removal

--- app/src/main/java/com/pong/grang/view/adapter/ActionReceiver.kt
+++ app/src/main/java/com/pong/grang/view/adapter/ActionReceiver.kt
@@ -28,7 +28,7 @@ class ActionReceiver(private val tts: TTSModel) : PlayerNotificationManager.Cust
         )
         val action1: NotificationCompat.Action =
             NotificationCompat.Action(
-                R.drawable.exo_icon_shuffle_on,
+                R.drawable.exo_icon_play,
                 "fav",
                 pendingIntent
             )
@@ -36,50 +36,10 @@ class ActionReceiver(private val tts: TTSModel) : PlayerNotificationManager.Cust
             HashMap()
         actionMap["fav"] = action1
         return actionMap
-
-        Log.d("wwww", "createeeee")
-//        val action =
-//            NotificationCompat.Action(
-//                context.resources
-//                    .getIdentifier("music_clear", "drawable", context.packageName),
-//                "closeBar",
-//                null
-//            )
-//        val actionMap: MutableMap<String, NotificationCompat.Action> =
-//            HashMap()
-//        actionMap["closeBar"] = action
-//        return actionMap
-//        val prevAction = Intent()
-//        prevAction.putExtra("action", actions[0])
-//        val backwardAction = Intent()
-//        backwardAction.putExtra("action", actions[1])
-//        val playAction = Intent()
-//        playAction.putExtra("action", actions[2])
-//        val pauseAction = Intent()
-//        pauseAction.putExtra("action", actions[3])
-//        val forwardAction = Intent()
-//        forwardAction.putExtra("action", actions[4])
-//        val nextAction = Intent()
-//        nextAction.putExtra("action", actions[5])
-//        return mutableMapOf(
-//            Pair(actions[0], NotificationCompat.Action(R.drawable.exo_icon_next, actions[0],
-//                PendingIntent.getBroadcast(context, 0, Intent(prevAction).setPackage(context?.packageName), PendingIntent.FLAG_CANCEL_CURRENT))),
-//            Pair(actions[1], NotificationCompat.Action(R.drawable.exo_icon_next, actions[1],
-//                PendingIntent.getBroadcast(context, 0, Intent(backwardAction).setPackage(context?.packageName), PendingIntent.FLAG_CANCEL_CURRENT))),
-//            Pair(actions[2], NotificationCompat.Action(R.drawable.exo_icon_play, actions[2],
-//                PendingIntent.getBroadcast(context, 0, Intent(playAction).setPackage(context?.packageName), PendingIntent.FLAG_CANCEL_CURRENT))),
-//            Pair(actions[3], NotificationCompat.Action(R.drawable.exo_icon_pause, actions[3],
-//                PendingIntent.getBroadcast(context, 0, Intent(pauseAction).setPackage(context?.packageName), PendingIntent.FLAG_CANCEL_CURRENT))),
-//            Pair(actions[4], NotificationCompat.Action(R.drawable.exo_icon_fastforward, actions[4],
-//                PendingIntent.getBroadcast(context, 0, Intent(forwardAction).setPackage(context?.packageName), PendingIntent.FLAG_CANCEL_CURRENT))),
-//            Pair(actions[5], NotificationCompat.Action(R.drawable.exo_icon_next, actions[5],
-//                PendingIntent.getBroadcast(context, 0, Intent(nextAction).setPackage(context?.packageName), PendingIntent.FLAG_CANCEL_CURRENT)))
-//        )
     }
 
     override fun onCustomAction(player: Player, action: String, intent: Intent) {
-        Log.d("wwww", action)
-        if(action.equals("fav")) {
+        if(action == "fav") {
             if (player.isPlaying) player.pause()
             else {
                 if (tts.ttsState == 1) {
