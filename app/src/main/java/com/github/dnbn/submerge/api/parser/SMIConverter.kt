4f948a99ddee7c4f7ef638eecb2ba2acc9622822
Snailpong <tjdtnsu@gmail.com>
2021-03-05 20:21:16 +0900

    Add: SMI Parsing (to be fixed)

--- app/src/main/java/com/github/dnbn/submerge/api/parser/SMIConverter.kt
+++ app/src/main/java/com/github/dnbn/submerge/api/parser/SMIConverter.kt
@@ -1,26 +1,53 @@
 package com.github.dnbn.submerge.api.parser
 
+import com.github.dnbn.submerge.api.subtitle.srt.SRTLine
+import org.mozilla.universalchardet.UniversalDetector
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
 import java.io.FileReader
+import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
 
+
 class SMIConverter() {
     companion object {
-        fun convertToFile(path : String) : String {
-            var line : String? = null
-            var lines : String = ""
-            val buf = BufferedReader(FileReader(path))
-            line = buf.readLine()
-            while(line != null) {
-                lines += line + '\n'
-                line = buf.readLine()
+        private fun findFileEncoding(file: File?): String? {
+            val buf = ByteArray(4096)
+            val fis = FileInputStream(file)
+
+            val detector = UniversalDetector(null)
+
+            var nread: Int
+            while (fis.read(buf).also { nread = it } > 0 && !detector.isDone) {
+                detector.handleData(buf, 0, nread)
             }
-//            val cs = StandardCharsets.UTF_8
-//            val list = Files.readAllLines(Paths.get(path), cs)
-//            return list.joinToString(separator = "\n")
-            return lines
+
+            detector.dataEnd()
+
+            val encoding = detector.detectedCharset
+            if (encoding != null) {
+                println("Detected encoding = $encoding")
+            } else {
+                println("No encoding detected.")
+            }
+
+            detector.reset()
+            return encoding
+        }
+
+        fun convertToFile(path : String) : String {
+            val cs = Charset.forName(findFileEncoding(File(path))!!)
+            val list = Files.readAllLines(Paths.get(path), cs)
+
+            val subLines = ArrayList<SRTLine>()
+
+            for (line in list) {
+                TODO()
+            }
+            return list.joinToString(separator = "\n")
         }
     }
 }
